ca94a90a4d12b321e7ebde5dfda6e4e81dc1493d
elene638 <elenejavaxishvili@yahoo.com>
2022-10-26 11:05:24 -0700

    Add note text character counter

--- src/components/NewNote.tsx
+++ src/components/NewNote.tsx
@@ -4,12 +4,37 @@ import {AddNoteProps} from "../interfaces/propsInterface"
 
 export const NewNote: React.FC<AddNoteProps> = ({addNotes}) => {
   const [noteText, setNoteText] = useState<string>("")
+  const length = 200
   // console.log(noteText)
 
   const handleClick = () => {
     // localStorage.setItem("note", JSON.stringify(noteText))
-    addNotes(noteText)
+    if (noteText.trim().length > 0) {
+      addNotes(noteText)
+      setNoteText("")
+    }
+    // setLength(0)
   }
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    
+    if(length - event?.target.value.length >= 0) {
+      setNoteText(event?.target.value)
+    }
+  }
+
+
+
+  return (
+    <div className='note new'>
+        <textarea onChange={handleChange} rows={8} value={noteText}  placeholder="Type to add a note..." />
+        <div className='note-footer' >
+            <p>{length - noteText.length} Remaining</p>
+            <button className='save-btn' onClick={handleClick}>Save</button>
+        </div>
+    </div>
+  )
+}
 
   // try {
   //   const result = JSON.parse(localStorage.getItem("note") || "");
@@ -20,15 +45,3 @@ export const NewNote: React.FC<AddNoteProps> = ({addNotes}) => {
   // }
 
   // console.log(JSON.parse(localStorage.getItem("note") || ""))
-
-
-  return (
-    <div className='note new'>
-        <textarea onChange={(event) => setNoteText(event?.target.value)} rows={8}  placeholder="Type to add a note..." />
-        <div className='note-footer' >
-            <p>words</p>
-            <button className='save-btn' onClick={handleClick}>Save</button>
-        </div>
-    </div>
-  )
-}
